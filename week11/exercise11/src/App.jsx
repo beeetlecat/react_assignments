c62b10ab1202797d37cfb0abda75e5a85f5811c9
beeetlecat <wyproctor@gmail.com>
2026-04-09 21:06:11 -0500

    Add files via upload

--- week11/exercise11/src/App.jsx
+++ week11/exercise11/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
+import './App.css';
 
 function SimpleCard({title, content}) {
     return (
         <div>
-            <h2>(title)</h2>
-            <p>(content)</p>
+            <h2>{title}</h2>
+            <p>{content}</p>
         </div>
     );
 }
 
 function App() {
-  return 
-  <div><h1>Hello React</h1>
-  <SimpleCard title="Hi Hi Hi" content="this is my first react project"></SimpleCard>
-  </div>
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <SimpleCard title="Hi Hi Hi" content="this is my first react project" />
+    </div>
+  );
 }
 
 export default App
